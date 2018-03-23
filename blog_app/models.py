f4776c32cc1bfcef045e8c8ad5e2c0be2cbdf48c
David <dtonlai@ualberta.ca>
2018-03-23 14:32:43 -0600

    Added date to BlogPost model

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 class UserProfileInfo(models.Model):
     user = models.OneToOneField(User)
@@ -11,6 +12,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=256)
     author = models.CharField(max_length=256)
     content = models.CharField(max_length=256)
+    date = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
         return self.title
