85440aea7bedd374ddf6e294facf720480cb559e
David <dtonlai@ualberta.ca>
2018-03-31 17:38:14 -0600

    Collapse navbar register forms boostrap

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -3,7 +3,21 @@ from django.contrib.auth.models import User
 from blog_app.models import models
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+        }
+    ))
+    email = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+        }
+    ))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+        }
+    ))
 
     class Meta:
         model = User
