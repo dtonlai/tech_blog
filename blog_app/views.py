c883e8be61bd8d31b82dd97e5a9d883aaffff4cc
David <dtonlai@ualberta.ca>
2018-04-07 15:59:47 -0600

    Fix commit

--- blog_app/views.py
+++ blog_app/views.py
@@ -45,11 +45,6 @@ class BlogDeleteView(DeleteView):
     success_url = reverse_lazy('tech_blog:list')
     template_name = 'blogpost_confirm_delete.html'
 
-class BlogPostCommentCreateView(CreateView):
-    fields = ('title', 'author', 'post')
-    model = models.BlogPostComment
-    template_name = 'blogpostcomment_form.html'
-
 def register(request):
     registered = False
 
