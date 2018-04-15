f952e66210670b42de18358a417540d4dde0d0ba
David <dtonlai@ualberta.ca>
2018-04-14 23:11:43 -0600

    Added timezone GMT

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 from django.core.urlresolvers import reverse
 
 from datetime import datetime
@@ -14,7 +15,8 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=256)
     author = models.CharField(max_length=256)
     content = models.CharField(max_length=256)
-    date = models.DateTimeField(default=datetime.now, blank=True)
+    create_date = models.DateTimeField(default=timezone.now, blank=True)
+    publish_date = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.title
